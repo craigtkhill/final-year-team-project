13618630ebd2795aa60b0dafe69a0ef8b4f70c93
Craig Hill <craigthomashill@gmail@.com>
2024-02-07 18:40:29 +0000

    add custom google font

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,5 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Entry point
-    </main>
-  );
+  return <></>;
 }
