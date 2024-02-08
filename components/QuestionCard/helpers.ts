9432543254f74c947ccef5728064c9706bff5ab9
Craig Hill <craigthomashill@gmail.com>
2024-02-08 20:24:38 +0000

    add help to dynamically add background colour to the answers

---
+++ components/QuestionCard/helpers.ts
@@ -0,0 +1,16 @@
+export const getBGColor = (
+  userAnswer: string | undefined,
+  correctAnswer: string,
+  answer: string
+) => {
+  const isAnswerCorrect = userAnswer ? userAnswer === correctAnswer : undefined;
+  if (
+    (isAnswerCorrect === true && userAnswer === answer) ||
+    (isAnswerCorrect === false && correctAnswer === answer)
+  )
+    return "bg-[#55ac78] text-white";
+  if (isAnswerCorrect === false && userAnswer === answer)
+    return "bg-[#ac5050] text-white";
+
+  return "bg-[#9f50ac] text-white";
+};
