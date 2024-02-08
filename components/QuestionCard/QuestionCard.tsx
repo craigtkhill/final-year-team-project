2fbfdc001e5220497ff019080809c3133000aa6d
Craig Hill <craigthomashill@gmail@.com>
2024-02-08 20:24:56 +0000

    add styling to the answers

--- components/QuestionCard/QuestionCard.tsx
+++ components/QuestionCard/QuestionCard.tsx
@@ -1,3 +1,5 @@
+import { getBGColor } from "./helpers";
+
 type Props = {
   currentQuestionIndex: number;
   question: string;
@@ -22,7 +24,15 @@ const QuestionCard = ({
     />
     <div className="flex flex-col items-center pt-8">
       {answers.map((answer) => (
-        <div key={answer} onClick={() => onClick(answer, currentQuestionIndex)}>
+        <div
+          key={answer}
+          className={`${getBGColor(
+            userAnswer,
+            correctAnswer,
+            answer
+          )} p-4 rounded w-full mb-4 cursor-pointer flex item-center justify-center select-none font-bold min-h-[45px] max-w-[400px] w-full my-2 rounded-[10px] hover:opacity-80 transition-opacity`}
+          onClick={() => onClick(answer, currentQuestionIndex)}
+        >
           <span
             className="truncate"
             dangerouslySetInnerHTML={{ __html: answer }}
