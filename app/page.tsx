75c18694a6d2d6f2a5f8ae3c757c27ec273b32fd
Craig Hill <craigthomashill@gmail@.com>
2024-02-16 17:53:00 +0000

    add splash page

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,20 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <p className="text-4xl font-bold text-center">Welcome to the Quiz!</p>
+    <div className="flex flex-col items-center justify-center h-screen space-y-8 px-4">
+      <p className="text-4xl font-bold text-center">Welcome to Eironauts!</p>
+      <p className="text-xl text-center">
+        The Eironauts are a force for good; we are all about reducing air
+        pollution and improving air quality. We’d like your help on our mission!
+        In this game, the aim is to develop your knowledge and awareness of air
+        pollution. To join us as a fellow Eironaut, however, you must first pass
+        the quiz! From there, you’ll be able to choose your character and go on
+        a journey through time and see how air pollution (or improving air
+        quality) could affect the world. When you’re ready, press the button
+        below to begin the quiz!
+      </p>
       {/* <Image className="w-24 h-24" src="/logo.svg" alt="Logo" /> */}
-      <Button text="Start Quiz" onClick={handleButtonClick} />
+      <Button text="Start Quiz" onClick={handleButtonClick} bgColor="#006400" />
     </div>
   );
 }
