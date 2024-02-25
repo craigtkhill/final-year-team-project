64a6c69b215c4b0faf2b113bff37add83d771d39
Craig Hill <craigthomashill@gmail@.com>
2024-02-25 12:57:21 +0000

    make basic results page to prevent error

--- app/quiz/result/page.tsx
+++ app/quiz/result/page.tsx
@@ -1,3 +1,14 @@
 // quiz/result/index.ts
 
-export {};
+import { NextPage } from "next";
+
+const ResultPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Result Page</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default ResultPage;
