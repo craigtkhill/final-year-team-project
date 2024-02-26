503ae28059d6b8f7048db2c8e7eac7aacca6ba60
Craig Hill <craigthomashill@gmail.com>
2024-02-26 15:22:48 +0000

    fix quiz test file

--- types/quiz.ts
+++ types/quiz.ts
@@ -1,14 +1,7 @@
-export enum Difficulty {
-  EASY = "easy",
-  MEDIUM = "medium",
-  HARD = "hard",
-}
-
 export type Question = {
   correct_answer: string;
   incorrect_answers: string[];
   question: string;
-  type: string;
   explanation: string;
 };
 
