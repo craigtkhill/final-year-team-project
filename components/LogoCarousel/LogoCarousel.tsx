6229b2590b275a5b49ab92049113c26a4d00ae1a
Craig Hill <craigthomashill@gmail.com>
2024-06-06 15:16:58 +0100

    Make logos static on larger tablet sized screens and dynamic on smaller screens

--- components/LogoCarousel/LogoCarousel.tsx
+++ components/LogoCarousel/LogoCarousel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/autoplay";
@@ -7,21 +7,26 @@ import Image from "next/image";
 import styles from "./LogoCarousel.module.css";
 
 const LogoCarousel: React.FC = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth >= 768); // Adjust the breakpoint as needed
+    };
+
+    handleResize(); // Call the function initially to set the initial state
+
+    window.addEventListener("resize", handleResize); // Listen for window resize events
+
+    return () => {
+      window.removeEventListener("resize", handleResize); // Clean up the event listener
+    };
+  }, []);
+
   return (
     <div className={`w-full overflow-hidden ${styles.carouselWrapper}`}>
-      <Swiper
-        modules={[Autoplay]}
-        spaceBetween={-150}
-        slidesPerView="auto"
-        autoplay={{
-          delay: 0,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: false,
-        }}
-        speed={5000}
-        loop={true}
-      >
-        <SwiperSlide className="flex justify-center items-center">
+      {isMobile ? (
+        <div className="flex flex-wrap justify-center space-x-24">
           <div className="relative w-[150px] h-[100px]">
             <Image
               src="/logos/applied-psychology-logo.png"
@@ -30,8 +35,6 @@ const LogoCarousel: React.FC = () => {
               alt="UCC Applied Psychology Logo"
             />
           </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex justify-center items-center">
           <div className="relative w-[150px] h-[100px]">
             <Image
               src="/logos/ucc-logo.png"
@@ -40,8 +43,6 @@ const LogoCarousel: React.FC = () => {
               alt="University College Cork Logo"
             />
           </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex justify-center items-center">
           <div className="relative w-[100px] h-[100px]">
             <Image
               src="/logos/behaviair-logo.png"
@@ -50,8 +51,52 @@ const LogoCarousel: React.FC = () => {
               alt="BehaviAir Logo"
             />
           </div>
-        </SwiperSlide>
-      </Swiper>
+        </div>
+      ) : (
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={-150}
+          slidesPerView="auto"
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: false,
+          }}
+          speed={5000}
+          loop={true}
+        >
+          <SwiperSlide className="flex justify-center items-center">
+            <div className="relative w-[150px] h-[100px]">
+              <Image
+                src="/logos/applied-psychology-logo.png"
+                fill
+                style={{ objectFit: "contain" }}
+                alt="UCC Applied Psychology Logo"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="flex justify-center items-center">
+            <div className="relative w-[150px] h-[100px]">
+              <Image
+                src="/logos/ucc-logo.png"
+                fill
+                style={{ objectFit: "contain" }}
+                alt="University College Cork Logo"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="flex justify-center items-center">
+            <div className="relative w-[100px] h-[100px]">
+              <Image
+                src="/logos/behaviair-logo.png"
+                fill
+                style={{ objectFit: "contain" }}
+                alt="BehaviAir Logo"
+              />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      )}
     </div>
   );
 };
